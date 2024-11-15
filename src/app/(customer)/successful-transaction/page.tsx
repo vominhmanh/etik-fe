843e7f9b80d0df4bc0cfb5fddd310e3941ddfb31
Võ Minh Mạnh (Jason) <VOMINHMANH29@GMAIL.COM>
2024-11-15 13:06:19 +0700

    update

--- src/app/(customer)/successful-transaction/page.tsx
+++ src/app/(customer)/successful-transaction/page.tsx
@@ -316,7 +316,7 @@ export default function Page(): React.JSX.Element {
                     <CardContent>
                       <Stack spacing={1} sx={{ display: 'flex', alignItems: 'center' }}>
                         <div style={{ width: '100px' }}>
-                          <img src={`https://api.qrserver.com/v1/create-qr-code/?size=100x100&data=${eCode}"`} />
+                          <img src={`https://api.qrserver.com/v1/create-qr-code/?size=100x100&data=${eCode}`} />
                         </div>
                         <Typography sx={{ textAlign: 'center' }}>{eCode}</Typography>
                       </Stack>
