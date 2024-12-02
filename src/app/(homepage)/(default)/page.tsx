2cae94a094e9d26815c964f5dc0d7ec98e4c4827
Võ Minh Mạnh (Jason) <VOMINHMANH29@GMAIL.COM>
2024-12-02 20:56:34 +0700

    update

--- src/app/(homepage)/(default)/page.tsx
+++ src/app/(homepage)/(default)/page.tsx
@@ -1,8 +1,3 @@
-export const metadata = {
-  title: "ETIK - Vé điện tử & Quản lý sự kiện",
-  description: "Phần mềm vé điện tử và quản lý sự kiện hiện đại, dễ dàng và tiết kiệm với tính năng tạo vé điện tử, gửi vé về email, quản lý sự kiện, check-in,...",
-};
-
 import Hero from "@/components/hero-home";
 import BusinessCategories from "@/components/business-categories";
 import FeaturesPlanet from "@/components/features-planet";
@@ -10,6 +5,19 @@ import LargeTestimonial from "@/components/large-testimonial";
 import Cta from "@/components/cta";
 import CreateYourEvent from "@/components/create-your-event";
 import CustomerAlbum from "@/components/customer-album";
+
+export const metadata = {
+  title: "ETIK - Vé điện tử & Quản lý sự kiện",
+  description:
+    "Phần mềm vé điện tử và quản lý sự kiện hiện đại, dễ dàng và tiết kiệm với tính năng tạo vé điện tử, gửi vé về email, quản lý sự kiện, check-in,...",
+  openGraph: {
+    title: 'ETIK - Vé điện tử & Quản lý sự kiện',
+    description: 'Phần mềm vé điện tử và quản lý sự kiện hiện đại, dễ dàng và tiết kiệm với tính năng tạo vé điện tử, gửi vé về email, quản lý sự kiện, check-in,...',
+    images: [{
+      url: '/assets/etik-logo1.png',
+    }],
+  }
+};
 
 export default function Home() {
   return (
